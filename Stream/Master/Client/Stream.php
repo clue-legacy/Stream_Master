a03fc5c7900c2e6d494db69bc6ad0e3511ae4d05
Christian Lück <christian@lueck.tv>
2011-06-01 15:23:11 +0200

    Properly use stream resource

--- Stream/Master/Client/Stream.php
+++ Stream/Master/Client/Stream.php
@@ -1,12 +1,22 @@
 <?php
 
 class Stream_Master_Client_Stream extends Stream_Master_Client{
+    /**
+     * stream resource
+     * 
+     * @var resource
+     */
+    protected $stream;
+    
     public function __construct($stream){
+        if(!is_resource($stream)){
+            throw new Stream_Master_Exception('Invalid stream resource given');
+        }
         $this->stream = $stream;
     }
     
     public function getNative(){
-        return $this->stream->getNative();
+        return $this->stream;
     }
     
     public function getStreamRead(){
